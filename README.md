b2b7ff245d1b5be7b297adce6b1200b1b089b571
Ryan Harter <harterrt@gmail.com>
2017-03-08 17:38:39 -0500

    Add very basic example ETL job

---
+++ README.md
@@ -0,0 +1,10 @@
+# What is this?
+
+This is a template for a Firefox Telemetry ETL job, meant to be used with [cookiecutter](https://cookiecutter.readthedocs.io/).
+This tool creates the boilerplate necessary to create a simple ETL job scheduled over [ATMO](https://analysis.telemetry.mozilla.org/)
+(including tests!!)
+
+
+# Appendix
+
+This work is inspired by [cookiecutter-python-cli](https://github.com/nvie/cookiecutter-python-cli)
---
+++ example_job/main.py
@@ -0,0 +1,38 @@
+from datetime import date
+from pyspark import Row
+
+from moztelemetry import get_pings_properties
+from moztelemetry.dataset import Dataset
+
+
+def get_data(sc):
+    pings = Dataset.from_source("telemetry") \
+        .where(docType='main') \
+        .where(submissionDate=date.today().strftime("%Y%m%d")) \
+        .where(appUpdateChannel="nightly") \
+        .records(sc, sample=0.1)
+
+    return get_pings_properties(pings, ["clientId",
+                                        "environment/system/os/name"])
+
+def ping_to_row(ping):
+    return Row(client_id = ping['clientId'],
+               os = ping['environment/system/os/name'])
+
+def transform_pings(pings):
+    """Take a dataframe of main pings and summarize OS share"""
+    out = pings.map(ping_to_row).distinct()\
+        .map(lambda x: x.os).countByValue()
+
+    return out
+
+def etl_job(sc, sqlContext):
+    """This is the function that will be executed on the cluster
+    """
+
+    results = transform_pings(get_data(sc))
+
+    # Print the OS and client_id counts in descending order:
+    for pair in sorted(results.items(), key=lambda x: -x[1]):
+        print pair
+
